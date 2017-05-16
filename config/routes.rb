5c92680390436f0aa383aec2e0581cbd6511d5cb
Sherilyn Moline <smoline@gmail.com>
2017-05-16 19:12:20 -0400

    All login options are working and the login display looks nice

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get "/auth/facebook/callback" => "users/omniauth_callbacks#facebook"
+    get "/auth/google/callback" => "users/omniauth_callbacks#google"
+    get "/auth/linkedin/callback" => "users/omniauth_callbacks#linkedin"
+    get "/auth/github/callback" => "users/omniauth_callbacks#github"
+  end
+
   get 'pages/landing'
   root 'pages#landing'
 
