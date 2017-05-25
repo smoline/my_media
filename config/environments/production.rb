7556941498ba91c46160490771525d7293254047
Sherilyn Moline <smoline@gmail.com>
2017-05-25 15:45:38 -0400

    devise settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "my_media_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: "my-media-library2.herokuapp.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
