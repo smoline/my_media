f5a7d0058caef8ad2b827ed15549abc108cc6299
Sherilyn Moline <smoline@gmail.com>
2017-05-25 14:17:35 -0400

    incorrect sendgrid setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,9 +85,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'https://my-media-library2.herokuapp.com',
