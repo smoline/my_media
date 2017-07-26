bd6d492f7755c2a264ae56c4bc12d0c0c095e097
Sherilyn Moline <smoline@gmail.com>
2017-07-26 12:44:41 -0400

    corrected tv_episode.rb attributes

--- app/models/tv_episode.rb
+++ app/models/tv_episode.rb
@@ -4,6 +4,7 @@ class TvEpisode < ApplicationRecord
   has_many :tv_guest_stars, dependent: :destroy
   has_many :people, through: :tv_guest_stars
 
-  has_many :tv_owners, dependent: :destroy
+  has_many :tv_owners, dependent: :destroy, inverse_of: :tv_episode
+  accepts_nested_attributes_for :tv_owners
   has_many :users, through: :tv_owners
 end
