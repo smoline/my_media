e61baa69b3fae889e8916aa131434ee5514a80d4
Sherilyn Moline <smoline@gmail.com>
2017-07-26 20:23:03 -0400

    tv favorites helper added

--- app/helpers/tv_favorites_helper.rb
+++ app/helpers/tv_favorites_helper.rb
@@ -1,2 +1,9 @@
 module TvFavoritesHelper
+  def tv_favorited(tvshowId)
+    if TvFavorite.where(:tv_show_id => tvshowId, :user_id => current_user.id).exists?
+      return true
+    else
+      return false
+    end
+  end
 end
