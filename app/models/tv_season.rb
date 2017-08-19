20b95a50be8a7578fb0ad64d2a974c3c710c26dd
Sherilyn Moline <smoline@gmail.com>
2017-08-19 11:49:13 -0400

    New TV Show nested form view updated

--- app/models/tv_season.rb
+++ app/models/tv_season.rb
@@ -5,4 +5,5 @@ class TvSeason < ApplicationRecord
   has_many :people, through: :tv_casts
 
   has_many :tv_episodes, dependent: :destroy
+  accepts_nested_attributes_for :tv_episodes, allow_destroy: true
 end
