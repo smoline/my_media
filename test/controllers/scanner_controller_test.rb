0bab5b962a76a5e7e77ef6f434bcdd06326c8b61
Sherilyn Moline <smoline@gmail.com>
2017-09-27 11:11:56 -0400

    Added corrected movie model tests

--- test/controllers/scanner_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ScannerControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -3,19 +3,19 @@ require 'test_helper'
 class MovieTest < ActiveSupport::TestCase
   # validates :title, presence: true
   test "should not save movie without title" do
-    @movie = Movie.new
+    @movie = Movie.new(tmdb_id: 5, description: "MyString", release_date: "1989-11-17", runtime: 1, tagline: "MyString", movie_image_url: "MyString")
     assert_not @movie.save, "Saved the movie without a title"
   end
 
   # validates :release_date, presence: true
   test "should not save movie without release date" do
-    @movie = Movie.new
+    @movie = Movie.new(title: "MyString", tmdb_id: 5, description: "MyString", runtime: 1, tagline: "MyString", movie_image_url: "MyString")
     assert_not @movie.save, "Saved the movie without a release_date"
   end
 
   # validates :tmdb_id, presence: true
   test "should not save movie without tmdb id" do
-    @movie = Movie.new
+    @movie = Movie.new(title: "MyString", description: "MyString", release_date: "1989-11-17", runtime: 1, tagline: "MyString", movie_image_url: "MyString")
     assert_not @movie.save, "Saved the movie without tmdb_id"
   end
 
